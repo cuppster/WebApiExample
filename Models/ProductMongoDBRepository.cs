aac7d2189205e7f74339eaa745edecc6290ad576
Jason Cupp <jason@cuppster.com>
2014-04-24 16:24:03 -0700

    add camel-case conventions to document field names

--- Models/ProductMongoDBRepository.cs
+++ Models/ProductMongoDBRepository.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using MongoDB.Driver;
 using MongoDB.Bson;
 using MongoDB.Driver.Builders;
+using MongoDB.Bson.Serialization.Conventions;
 
 namespace Cuppster.WebApiExample.Models
 {
@@ -14,22 +15,30 @@ namespace Cuppster.WebApiExample.Models
 
 		public ProductMongoDBRepository ()
 		{
+			// fields whould start with lower-case
+			var camelCaseConventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+			ConventionRegistry.Register("CamelCase", camelCaseConventionPack, type => true);
 
+			// setup connections
 			var connection = "mongodb://localhost:27017";
 			var client = new MongoClient(connection);
 			var server = client.GetServer();
-			var _database = server.GetDatabase ("Products");
-			_products = _database.GetCollection<Product> ("products");
+			var database = server.GetDatabase ("Products");
+
+			// collection
+			_products = database.GetCollection<Product> ("products");
 
 			// init
 			_products.RemoveAll ();
 
+			// mock products
 			Product[] mockProducts = new Product[] { 
 				new Product { Name = "Tomato Soup", Category = "Groceries", Price = 1 }, 
 				new Product { Name = "Yo-yo", Category = "Toys", Price = 3.75M }, 
 				new Product { Name = "Hammer", Category = "Hardware", Price = 16.99M } 
 			};
 
+			// add mock products to collection
 			foreach (var product in mockProducts) {
 				AddProduct (product);
 			}
